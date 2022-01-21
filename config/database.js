e3aeb66ba66442c010872600a6d921e450f2ffa8
jhon-innovarweb <gkpp1ye9r@mozmail.com>
2022-01-21 16:25:23 -0500

    prueba

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', 'the-healthy-treatments-test.cl5lkemfe3j1.us-east-2.rds.amazonaws.com'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'the_healthy_treatment_test_db'),
-        username: env('DATABASE_USERNAME', 'innovarweb'),
-        password: env('DATABASE_PASSWORD', 'jjVZAf*%ZxUj'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', '!root0151'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
