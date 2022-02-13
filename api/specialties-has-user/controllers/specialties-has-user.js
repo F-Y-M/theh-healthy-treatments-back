4d285c3d9af106a0d674714c72e001cf5ca839fd
F-Y-M <jan.juan@misena.edu.co>
2022-02-13 17:34:08 -0500

    feat: se agrega consulta para specialties junto con specialties_has_users

--- api/specialties-has-user/controllers/specialties-has-user.js
+++ api/specialties-has-user/controllers/specialties-has-user.js
@@ -5,4 +5,21 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async specialtyHasUsersSearch(ctx) {
+        const { professional_id } = ctx.request.query;
+        const query =
+        `SELECT 
+          shu.id specialties_has_user_id,
+          shu.consultation_value,
+          specialty.id specialty_id,
+          specialty.specialty speciality
+        FROM specialties_has_users shu
+          LEFT JOIN specialties as specialty on specialty.id = shu.specialty_id \n` +
+          `WHERE shu.professional_id = ${professional_id}
+        GROUP BY shu.id;`;
+        const result = await strapi.connections.default.raw(query);
+        return result[0];
+      },
+    
+};
