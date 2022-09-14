b109210c83099615d9b3d30a03c5bd183720589c
Carlos Muñoz <info@innovarweb.com>
2022-09-14 15:56:01 +0000

    se agrega lenguajes

--- config/database.js
+++ config/database.js
@@ -5,15 +5,15 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        database: 'tht_test',
-        username: 'root',
-        password: '!1Contraseña',
-        ssl: false  
-        // {
-        //   rejectUnauthorized: env.bool('DATABASE_SSL', false), // For self-signed certificates
-        // },
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'the_healthy_treatment_test_db'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', '!1Contraseña'),
+        ssl:  
+        {
+          rejectUnauthorized: env.bool('DATABASE_SSL', false), // For self-signed certificates
+        },
       },
       options: {}
     },
