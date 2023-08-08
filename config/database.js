3d6315cb938dcbe84f7d7e974b4f043b1af507fb
Carlos Muñoz <info@innovarweb.com>
2023-08-08 15:02:45 +0000

    fix(email): se agegan rutas para el envio de mensajes por medio de api.
    se agregan rutas a email para el uso en api

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'the_healthy_treatment_test_db'),
+        database: env('DATABASE_NAME', 'tht_pro'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', '!1Contraseña'),
         ssl:  
